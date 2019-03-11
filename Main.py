2b3faa25a7f31678260b954fc17a1a71a89bb3bf
Trevor Peitzman <trevor.peitzman@gmail.com>
2019-03-11 11:55:14 -0700

    Finished Numeric Poll function. Testing Alpha Poll

--- Main.py
+++ Main.py
@@ -1,2 +1,15 @@
 
+import pollUsr.poll as poll
 
+def roughtest():
+
+    age = poll.numeric_metric('age', 'years', 1, 100)
+    height = poll.numeric_metric('height', 'inches', 24, 96)
+    weight = poll.numeric_metric('weight', 'pounds', 80, 400)
+    sex = poll.alpha_metric('gender', 'M/F')
+
+    print str(age) + sex + weight
+    return
+
+
+roughtest()
---
+++ pollUsr/poll.py
@@ -0,0 +1,34 @@
+def numeric_metric(desired_metric, units, lobound, hibound):
+    """Prompts user to provide a number 'desired_metric' with the units 'units'.
+    Before returning user input, function validates number is in range using
+    'lobound' and 'hibound' respectively.
+    desired_metric -> string
+    units -> string
+    lobound -> string
+    hibound -> string"""
+
+    while True:
+        usr_input = raw_input('Enter your ' + desired_metric + ' (' + units + '): ')
+        try:
+            int(usr_input) == usr_input
+        except:
+            print('Entry non-numeric / out of range.\nTry again.')
+        else:
+            if lobound < int(usr_input) < hibound and usr_input != str():
+                return usr_input
+            else:
+                print('Entry non-numeric / out of range.\nTry again.')
+
+
+def alpha_metric(desired_metric, units):
+    """Prompts user to provide a string 'desired_metric' with the units 'units'.
+    Before returning user input, function validates input is a string.
+    desired_metric -> string
+    units -> string"""
+
+    while True:
+        usr_input = raw_input('Enter your ' + desired_metric + ' (' + units + '): ')
+        if usr_input == str():
+            return usr_input
+        else:
+            print('Entry not a string.\nTry again.')
