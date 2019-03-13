2236b534baf5f39dda5930e5e9624ea2711fff2a
Brandon A. Rios <44213901+BrandonRios@users.noreply.github.com>
2019-03-13 12:57:31 -0700

    Finished Hydration Calculation

--- Main.py
+++ Main.py
@@ -1,17 +1,18 @@
-
 import utils.poll as poll
 import utils.conversions as conversions
 
-def macrocalc():
 
+def macrocalc():
     age = poll.numeric_metric('age', 'years', 1, 100)
     height = poll.numeric_metric('height', 'inches', 24, 96)
     weight = poll.numeric_metric('weight', 'pounds', 80, 400)
     sex = poll.alpha_metric('gender', 'M/F')
 
     print conversions.bmi(height, weight)
+    print 'Optimal Hydration Level: ' + str(conversions.hydration(weight, age)) + ' liquid ounces/day'
     print (age, sex, weight)
+
     return
 
-macrocalc()
 
+macrocalc()
--- utils/conversions.py
+++ utils/conversions.py
@@ -2,8 +2,20 @@ def bmi(height, weight):
     output = 703 * (float(weight)/(height**2))
     return float(output)
 
-def hydration(weight, variable):
-    output = (float(weight / 2.2) * variable)
+def hydration(weight, age):
+
+    if age < 30:
+        variable = 40
+    elif age > 30 and age < 55:
+        variable = 35
+    else:
+        variable = 30
+
+    precursor = (float(weight / 2.2) * variable)
+    output = (precursor / 28.3)
+    return round(float(output), 2)
+
+
 
 
 
