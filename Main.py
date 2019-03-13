f16ae4bb6ba46f3bae172e7f7de801f792de0d29
Trevor Peitzman <trevor.peitzman@gmail.com>
2019-03-13 10:23:51 -0700

    Added color to intromessage

--- Main.py
+++ Main.py
@@ -4,14 +4,15 @@ import utils.conversions as conversions
 import time
 import sys
 
-intromessage = 'Hello and welcome to MacroCalc. This program will inform you of your optimal levels of nutritional' \
-                '\nmacronutrients, vitamins, minerals, and hydration. First, we will begin by asking you a few' \
-                '\nquestions about yourself. Let us begin...\n'
+intromessage = '\033[1;34;m Hello and welcome to MacroCalc. This program will inform you of your optimal levels of' \
+                '\nnutritional macronutrients, vitamins, minerals, and hydration. First, we will begin by asking you' \
+                '\na few questions about yourself. Let us begin...\n \033[1;37;m'
 
 def macrocalc():
 
     for letter in intromessage:
         sys.stdout.write(letter)
+        sys.stdout.flush()
         time.sleep(.05)
 
     age = poll.numeric_metric('age', 'years', 1, 100)
