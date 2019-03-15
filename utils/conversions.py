fb9f6e4ae950d4fc06d250f5e5b4f9aa365b69a0
Trevor Peitzman <trevor.peitzman@gmail.com>
2019-03-15 13:11:24 -0700

    FIrst Pass of Calcium Function

--- utils/conversions.py
+++ utils/conversions.py
@@ -2,6 +2,7 @@ def bmi(height, weight):
     output = 703 * (float(weight)/(height**2))
     return float(output)
 
+
 def hydration(weight, age):
 
     if age < 30:
@@ -16,6 +17,30 @@ def hydration(weight, age):
     return round(float(output), 2)
 
 
+def calcium(age, sex):
+
+    input = int(age)
+
+    if input < 1:
+        return '260 mg'
+    elif 1 <= input <= 3:
+        return '700 mg'
+    elif 4 <= input <= 8:
+        return '1000 mg'
+    elif 9 <= input <= 18:
+        return '1300 mg'
+    elif 19 <= input <= 50:
+        return '1000 mg'
+    elif 51 <= input <= 70:
+        if sex == 'M':
+            return '1000 mg'
+        else:
+            return '1200 mg'
+    elif 71 <= input:
+        return '1200 mg'
+    else:
+        print 'calcium unknown'
+        return 'calcium unknown'
 
 
 
