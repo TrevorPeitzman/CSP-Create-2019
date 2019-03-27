8757e00598722cb08d9a3e35b2ff2e27093d3571
Trevor Peitzman <trevor.peitzman@gmail.com>
2019-03-27 09:36:00 -0700

    Added comments to main file

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import utils.conversions as conversions
 import time
 import sys
 
+# Establish welcome message as a variable
 intromessage = '\033[1;34;m Hello and welcome to MacroCalc. This program will inform you of your optimal daily levels' \
                 '\nof nutritional macronutrients, vitamins, minerals, and hydration. First, we will ask you' \
                 '\na few questions about yourself. Let us begin...\n \033[1;37;m'
@@ -10,16 +11,20 @@ intromessage = '\033[1;34;m Hello and welcome to MacroCalc. This program will in
 
 def macrocalc():
 
+    # Extra feature - the intromessage is "typed" on screen for added effect
+    # sourced from https://stackoverflow.com/questions/9246076/how-to-print-one-character-at-a-time-on-one-line
     for letter in intromessage:
         sys.stdout.write(letter)
         sys.stdout.flush()
         time.sleep(.05)
 
+    # using our created libraries and utilities, poll the user for their data
     age = poll.numeric_metric('age', 'years', 1, 100)
     height = poll.numeric_metric('height', 'inches', 24, 96)
     weight = poll.numeric_metric('weight', 'pounds', 80, 400)
     sex = poll.alpha_metric('gender', 'M/F')
 
+    # Once done polling, calculate and print the results
     print '\nYour BMI is: ' + str(round(conversions.bmi(height, weight), 2))
     print conversions.magnesium(age, sex)
     print conversions.calcium(age, sex)
