f5bdf5d292a85173fe2a7a49f243c847ea231438
Trevor Peitzman <trevor.peitzman@gmail.com>
2019-03-27 09:46:46 -0700

    Finish commenting poll.py

--- utils/poll.py
+++ utils/poll.py
@@ -8,15 +8,20 @@ def numeric_metric(desired_metric, units, lobound, hibound):
     hibound -> string"""
 
     while True:
+        # Poll user forever
         usr_input = raw_input('Enter your ' + desired_metric + ' (' + units + '): ')
         try:
+            # Attempt to cast the input to an integer
             int(usr_input) == usr_input
         except:
+            # If casting fails, print
             print('Entry non-numeric / out of range.\nTry again.')
         else:
+            # return the input if it fits within the bounds
             if lobound < int(usr_input) < hibound and usr_input != str():
                 return int(usr_input)
             else:
+                # otherwise print an error
                 print('Entry non-numeric / out of range.\nTry again.')
 
 
